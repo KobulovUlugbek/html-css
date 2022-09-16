6efa14625e80d34036205488749d5b04aa361e36
KobulovUlugbek <108155304+KobulovUlugbek@users.noreply.github.com>
2022-09-16 18:03:48 +0200

    Add files via upload
    HTML and CSS Codes with Photos.  Disign.

---
+++ Portfolio/function.js
@@ -0,0 +1,29 @@
+var a;
+
+// For Biography
+function show_hide()
+{
+    if(a==1){
+        document.getElementById("lorem").style.display="inline";
+        return a=0;
+    }
+
+    else {
+        document.getElementById("lorem").style.display="none";
+        return a=1;
+    }
+}
+
+// For Education
+function show_hide()
+{
+    if(a==1){
+        document.getElementById("lorem").style.display="inline";
+        return a=0;
+    }
+
+    else {
+        document.getElementById("lorem").style.display="none";
+        return a=1;
+    }
+}
